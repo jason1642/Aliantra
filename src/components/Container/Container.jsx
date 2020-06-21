92a983b7fb19105f498416045ea888bb8c4e25bf
jasonc1642 <jason1642@gmail.com>
2020-06-21 02:27:39 -0400

    getting started on tiingo api

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -15,14 +15,38 @@ class Container extends Component {
     }
   }
   async componentDidMount() {
-    const DOMAIN = 'https://finnhub.io/api/v1/quote?symbol=';
-    const NAME = 'AAPL'
-    const API_KEY = 'brlva87rh5re8ma1oe5g'
-    const response = await axios(`${DOMAIN}${NAME}&token=${API_KEY}`)
-    this.setState({
-      stockAPI: response.data
-    })
-    console.log(response.data)
+    // 'stock/profile2'
+    // '/quote'
+
+
+
+
+    const TIINGO_API_KEY = '458b98b2a987d2fba65f5be43ca8f1090488992b'
+    const TIINGO_DOMAIN = 'https://api.tiingo.com/tiingo/daily/aapl?token='
+    let marketArr = []
+    const marketSymbols = ['SPY', 'QQQ', 'IWM', 'DIA']
+    // marketSymbols.forEach(async function (e) {
+    //   const marketResponse = await axios(`${TIINGO_DOMAIN}${e}?token=${TIINGO_API_KEY}`)
+    //   marketArr.push(marketResponse.data)
+    // })
+    // const marketResponse = await axios(`${TIINGO_DOMAIN}${TIINGO_API_KEY}`)
+    // console.log(marketResponse)
+
+
+
+
+
+
+
+    // const DOMAIN = 'https://finnhub.io/api/v1/stock/profile2?isin=';
+    // const NAME = 'US78462F1030'
+    // const API_KEY = 'brlva87rh5re8ma1oe5g'
+
+    // const response = await axios(`${DOMAIN}${NAME}&token=${API_KEY}`)
+    // this.setState({
+    //   stockAPI: response.data
+    // })
+    // console.log(response.data)
   }
   render() {
     return (<>
--- src/components/MarketsBox/MarketsBox.jsx
+++ src/components/MarketsBox/MarketsBox.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 const MarketsBox = (props) => {
+  const marketsStrings = ['S&P 500', 'Nasdaq', 'Russell 2K', 'Dow']
   return (<div className='markets-box'>
     <h1>Markets</h1>
-    <div>
-      {/* {props.stockAPI.companyName} */}
-    </div>
+    {marketsStrings.map(e =>
+      <div>{e}</div>
+    )}
+
   </div>);
 }
 
